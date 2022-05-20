2f1f4925990a77db9d054602c5d299999252fd88
Fred Luetkemeier <fredluetkemeier@gmail.com>
2022-05-20 15:54:29 -0500

    Finished lesson 3

--- lesson3/lesson3.spec.js
+++ lesson3/lesson3.spec.js
@@ -3,65 +3,69 @@ const R = require('ramda');
 describe('Practice what we learned', () => {
   it('should get the value of obj.foo', () => {
     const obj = { foo: 'bar' };
-    const res = R; // your work here
+    const res = R.prop('foo', obj); // your work here
 
     expect(res).toBe('bar');
   });
 
-  xit('should set the value of obj.foo to "baz"', () => {
+  it('should set the value of obj.foo to "baz"', () => {
     const obj = { foo: 'bar' };
-    const res = R; // your work here
+    const res = R.assoc('foo', 'baz', obj); // your work here
 
     expect(res).toEqual({ foo: 'baz' });
   });
 
-  xit('should get the value of obj.foo.bar', () => {
+  it('should get the value of obj.foo.bar', () => {
     const obj = { foo: { bar: 'baz' } };
-    const res = R; // your work here
+    const res = R.path(['foo', 'bar'], obj); // your work here
 
     expect(res).toBe('baz');
   });
 
-  xit('should set the value of obj.foo.bar to "biz"', () => {
+  it('should set the value of obj.foo.bar to "biz"', () => {
     const obj = { foo: { bar: 'baz' } };
-    const res = R; // your work here
+    const res = R.assocPath(['foo', 'bar'], 'biz', obj); // your work here
 
     expect(res).toEqual({ foo: { bar: 'biz' } });
   });
 
-  xit('should set obj.hello to "WORLD"', () => {
+  it('should set obj.hello to "WORLD"', () => {
     const obj = { hello: 'world' };
-    const res = R; // your work here
+    const res = R.assoc('hello', 'WORLD', obj); // your work here
 
     expect(res).toEqual({ hello: 'WORLD' });
   });
 
-  xit('should default obj.foo to "unknown"', () => {
+  it('should default obj.foo to "unknown"', () => {
     const obj = { hello: 'world' };
-    const res = R; // your work here
+    const res = R.propOr('unknown', 'foo', obj); // your work here
 
     expect(res).toBe('unknown');
   });
 
-  xit('should default obj.foo.bar to "unknown"', () => {
+  it('should default obj.foo.bar to "unknown"', () => {
     const obj = { hello: 'world' };
-    const res = R; // your work here
+    const res = R.pathOr('unknown', ['foo', 'bar'], obj); // your work here
 
     expect(res).toBe('unknown');
   });
 });
 
-xdescribe('Review', () => {
+describe('Review', () => {
   it('should get the value of obj.x and add three', () => {
     const obj = { x: 4 };
-    const res = R; // your work here
+    const res = R.pipe(R.prop('x'), R.add(3))(obj); // your work here
 
     expect(res).toBe(7);
   });
 
-  xit('should set the value of obj.x.y to obj.x.y times 2 plus 5', () => {
+  it('should set the value of obj.x.y to obj.x.y times 2 plus 5', () => {
     const obj = { x: { y: 7 } };
-    const res = R; // your work here
+
+    const xyLens = R.lensPath(['x', 'y']);
+    const times2Plus5 = R.pipe(R.multiply(2), R.add(5));
+
+    const res = R.over(xyLens, times2Plus5)(obj); // your work here
 
     expect(res).toEqual({ x: { y: 19 } });
   });
